27644b000c825c704b806b04e6ca95e8242fae4f
Threephop <150276224+Threephop@users.noreply.github.com>
2025-04-22 20:39:51 +0700

    Revert "routers"
    This reverts commit 1d791ddd97c730e9999442af68099651348bcb73.

--- backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const route = express.Router();
-
-route.get('/login', (req, res) => {
-    res.send('Login route');
-});
-
-export default route;
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js';
- 
-const app = express();
+const express = require('express');
 
-dotenv.config();
-const PORT = process.env.PORT || 5000;
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use("/api/auth", authRoutes);
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
