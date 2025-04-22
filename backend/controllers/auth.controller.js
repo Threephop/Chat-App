d8200280569aff5a9690eecc243b99b644ba8ad4
Threephop <150276224+Threephop@users.noreply.github.com>
2025-04-22 22:32:13 +0700

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,14 @@
 export const login = (req, res) => {
     console.log("Login User");
+    res.send("Login User");
 }
 
 export const logout = (req, res) => {
     console.log("Logout User");
+    res.send("Logout User");
 }
 
 export const signup = (req, res) => {
     console.log("signup User");
+    res.send("signup User");
 }
