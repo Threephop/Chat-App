e0b0d2ee6906a19077cbe52300cd952dbb45cd87
Threephop <150276224+Threephop@users.noreply.github.com>
2025-04-22 20:53:28 +0700

    controllers

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { signup, login, logout } from '../controllers/auth.controller.js';
 
 const route = express.Router();
 
-route.get('/login', (req, res) => {
-    res.send('Login route');
-});
+route.get('/signup', signup);
+
+route.get('/login', login);
+
+route.get('/logout', logout);
 
 export default route;
