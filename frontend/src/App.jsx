eb2af9eb85204b6a04403cb8f1275da3da2beadd
Threephop <150276224+Threephop@users.noreply.github.com>
2025-07-07 20:40:41 +0700

    home ux/ui ver1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/login'
 import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
 
 function App() {
   return <div className='p-4 h-screen flex items-center justify-center'>
-    <SignUp />
+    <Home />
   </div>
 }
 
